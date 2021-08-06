bd7cd54577bad4ceeac75bba980a5e2192b24fb3
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-06 16:15:35 +0200

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-module.exports = function toReadable(number) {
- const arr = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve', 'thirteen ','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-return arr[number]
+module.exports = function toReadable(num) {
+  const arr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen ', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const arrDozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  const hundredthCount=Math.floor(num/100)
+  const tenthsCount = Math.floor(num / 10 % 10)
+  const unitsCount = num % 10
+  
+    if (num === 0) {
+    return 'zero'
+}else if (num < 20) {
+    return arr[num]
+  } else if ( num<=99) {
+        return `${arrDozens[tenthsCount]}  ${arr[unitsCount]}`
+  } else if (num <= 999) {
+
+    return `${arr[hundredthCount]} hundred ${arrDozens[tenthsCount]} ${arr[unitsCount]} `
+  }
 }
